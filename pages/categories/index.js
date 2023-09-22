1f456536a7e1e2f59b01eb8d6000f3b012696378
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-22 17:17:28 +0330

    filter-food

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,7 +1,46 @@
 import CategoriesPage from "@/components/template/CategoriesPage";
 
-const Categories = () => {
-  return <CategoriesPage />;
+const Categories = ({data}) => {
+  return <CategoriesPage {...data} />;
 };
 
 export default Categories;
+
+export async function getServerSideProps(context) {
+  const {
+    query: { difficulty, time },
+  } = context;
+
+  const response = await fetch("http://localhost:4000/data");
+  const data = await response.json();
+
+  const filtredData = data.filter((item) => {
+    const difficultyResult = item.details.filter(
+      (detail) => detail.Difficulty && detail.Difficulty === difficulty
+    );
+
+    const timeResult = item.details.filter((detail) => {
+      const cookingTime = detail["Cooking Time"] || "";
+      const [timeDetail] = cookingTime.split(" ");
+      if (time === "less" && timeDetail && +timeDetail <= 30) {
+        return detail;
+      } else if (time === "more" && +timeDetail > 30) {
+        return detail;
+      }
+    });
+
+    if (time && difficulty && timeResult.length && difficultyResult.length) {
+      return item;
+    } else if (!time && difficulty && difficultyResult.length) {
+      return item;
+    } else if (time && !difficulty && timeResult.length) {
+      return item;
+    }
+  });
+
+  return {
+    props: {
+      data: filtredData,
+    },
+  };
+}
