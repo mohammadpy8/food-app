a1983f6d4088ebb7ce0cdf41a9fdcbe3f4334135
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-22 14:02:32 +0330

    fetch-dynamic-route

--- pages/menu/[menuId].js
+++ pages/menu/[menuId].js
@@ -1,4 +1,9 @@
-const MenuDetails = () => {
+import { useRouter } from "next/router";
+
+const MenuDetails = ({ foodDetail }) => {
+  const router = useRouter();
+  if (router.isFallback) return <h2>Is Loading...</h2>;
+
   return <div></div>;
 };
 
@@ -12,12 +17,28 @@ export async function getStaticPaths() {
 
   const paths = data.map((food) => ({
     params: {
-      id: food.id.toString(),
+      menuId: food.id.toString(),
     },
   }));
 
   return {
-      paths,
-      fallback: true,
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { menuId },
+  } = context;
+
+  const response = await fetch(`http://localhost:4000/data/${menuId}`);
+  const data = await response.json();
+  if (!data.id) return { notFound: true };
+  return {
+    props: {
+      foodDetail: data,
+    },
+    revalidate: 10,
   };
 }
