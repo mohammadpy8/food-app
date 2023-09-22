54b63f035d9b1fc9df804d8f3c4d631cba4e095d
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-22 11:07:46 +0330

    fetchApiFood

--- components/template/MenuPage.js
+++ components/template/MenuPage.js
@@ -1,12 +1,16 @@
 import style from "./MenuPage.module.css";
 
 const MenuPage = ({ data }) => {
-    
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className={style.container}>
+      <h2>Menu</h2>
+      <div className={style.subContainer}>
+        {data.map((food) => (
+          <p key={food.id}>{food.name}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuPage;
