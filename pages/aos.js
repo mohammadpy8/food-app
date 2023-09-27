5d3ffd04a14e819bd33de284d706545ba0358385
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-27 11:56:30 +0330

    hh

--- pages/aos.js
+++ pages/aos.js
@@ -1,30 +1,45 @@
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { SkeletonTheme } from "react-loading-skeleton";
-import { useSearchParams, usePathname, useParams } from "next/navigation";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 const Aos = () => {
-  const searchParams = useSearchParams();
-  const search = searchParams.get('search');
-
-  const name = usePathname();
-  const splitedName = name.split("/")[1];
-  console.log(splitedName);
-  const params = useParams();
-  console.log(params);
-
-  console.log(search);
-
-  useEffect(() => {
-    AOS.init();
-    AOS.refresh();
-  }, []);
+  const signup = Yup.object().shape({
+    firstName: Yup.string().min(2, "short!").max(20, "long!!").required("req"),
+    lastName: Yup.string().min(2, "short!").max(20, "long!!").required("req"),
+    email: Yup.string().email("email").required("e"),
+  });
 
   return (
-    <SkeletonTheme baseColor="#808080" highlightColor="#b1b1b1">
-      <div data-aos="fade-up">Animate me!</div>
-    </SkeletonTheme>
+    <div>
+      <h1>Signup</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        validationSchema={signup}
+        onSubmit={(values) => {
+          // same shape as initial values
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field name="firstName" />
+            {errors.firstName && touched.firstName ? (
+              <div>{errors.firstName}</div>
+            ) : null}
+            <Field name="lastName" />
+            {errors.lastName && touched.lastName ? (
+              <div>{errors.lastName}</div>
+            ) : null}
+            <Field name="email" type="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
